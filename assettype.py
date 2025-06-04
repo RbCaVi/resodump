ae03313afce790411fd73eb7be99e67834c4b0b6
RbCaVi <robert@vail-family.com>
2025-06-04 10:18:40 -0400

    missed ogg type

--- assettype.py
+++ assettype.py
@@ -1,4 +1,4 @@
-# try to fine resonite asset types by magic numbers
+# try to find resonite asset types by magic numbers
 # i know of five types of assets:
 #   image - png or webp format - .bitmap metadata
 #   sound - ogg or wav
@@ -7,15 +7,18 @@
 #   object - resonite frdt format - only one in a .resonitepackage as far as i know
 
 def assettype(data):
-  # these first four are from https://en.wikipedia.org/wiki/List_of_file_signatures
+  # these first five are from https://en.wikipedia.org/wiki/List_of_file_signatures
   if data[:8] == b'\x89PNG\r\n\x1a\n':
     return ('bitmap', 'png')
   if data[:4] == b'RIFF' and data[8:12] == b'WEBP':
     return ('bitmap', 'webp')
   if data[:4] == b'RIFF' and data[8:12] == b'WAVE':
     return ('sound', 'wav')
+  if data[:4] == b'OggS':
+    return ('sound', 'ogg')
   if data[:5] == b'\x00\x01\x00\x00\x00':
     return ('font', 'ttf')
+  # these are custom formats from resonite (neosvr?)
   if data[:4] == b'FrDT':
     return ('object', 'frdt')
   if data[:6] == b'MeshX\x05':
