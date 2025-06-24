6b9aa1285eb796489a4d8159179f788504bf556c
RbCaVi <robert@vail-family.com>
2025-06-24 12:55:58 -0400

    fix decodemeshes.py

--- decodemeshes.py
+++ decodemeshes.py
@@ -4,9 +4,11 @@
 import os
 import json
 import sys
+import io
 
 import resonitepackage
 import meshx
+import lz4
 
 #packagename = 'Redprint Manager 2.4.zip'
 #packagename = 'out/inventory-tool.resonitepackage'
@@ -19,13 +21,13 @@ with resonitepackage.ResonitePackage(packagename) as package:
     if not data.startswith(b'\x05MeshX'):
       # i don't know if this is worth it (checking if it's a compressed meshx file)
       try:
-        data = lz4.lz4decompress(data)
+        data = lz4.lz4decompress(io.BytesIO(data))
+        if not data.read(6) == b'\x05MeshX':
+          print('nope')
+          continue
       except:
         # there's only one place that can throw exceptions, right?
         # (i'm assuming all errors here come from lz4.lz4decompress())
-        print('nope')
-        continue
-      if not data.startswith(b'\x05MeshX'):
         print('nope')
         continue
     print(f'yes, {len(data)} bytes')
