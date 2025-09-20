b7e9e0d9519a0887a913a0fb3bea944abf66414d
RbCaVi <robert@vail-family.com>
2025-09-19 22:41:11 -0400

    add a bunch of validation to pf_metadata.py
    also don't print some redundant fields (Index and Field)

--- pf_metadata.py
+++ pf_metadata.py
@@ -6,9 +6,19 @@ def lstr(l):
     *[indent(str(x)) for x in l],
   ']'))
 
+def assertfieldattributes(x, a):
+  if not isinstance(a, list):
+    a = [a]
+  assert x.Field is None or int(x.Field.Attributes) in a, f'{x.Field.Attributes} == {hex(x.Field.Attributes)} not in {[hex(x) for x in a]}'
+
+def assertfieldtype(x, attr):
+  assert x.Field is None or getattr(x, attr) == x.Field.FieldType.GenericTypeArguments[0]
+
 # convert a C# NodeMetadata object into a nested tuple of the data inside it
 class ProcessNodeMetadata:
   def __init__(self, meta):
+    self.Name = meta.Name
+    self.Overload = meta.Overload
     self.IsPotentiallyListeningToChanges = meta.IsPotentiallyListeningToChanges
     self.FixedInputs = [*map(ProcessInputMetadata, meta.FixedInputs)]
     self.FixedOutputs = [*map(ProcessOutputMetadata, meta.FixedOutputs)]
@@ -21,9 +31,16 @@ class ProcessNodeMetadata:
     self.DynamicImpulses = [*map(ProcessImpulseListMetadata, meta.DynamicImpulses)]
     self.DynamicOperations = [*map(ProcessOperationListMetadata, meta.DynamicOperations)]
     self.DynamicGlobalRefs = [*map(ProcessGlobalRefListMetadata, meta.DynamicGlobalRefs)]
+    
+    for l in [self.FixedInputs, self.FixedOutputs, self.FixedImpulses, self.FixedOperations, self.FixedReferences, self.FixedGlobalRefs, self.DynamicInputs, self.DynamicOutputs, self.DynamicImpulses, self.DynamicOperations, self.DynamicGlobalRefs]:
+      for i,x in enumerate(l):
+        assert x.Index == i
+    assert len(self.DynamicGlobalRefs) == 0
   
   def __str__(self):
     return '\n'.join(('NodeMetadata {',
+      indent('Name = ' + str(self.Name)),
+      indent('Overload = ' + str(self.Overload)),
       indent('IsPotentiallyListeningToChanges = ' + str(self.IsPotentiallyListeningToChanges)),
       indent('FixedInputs = ' + lstr(self.FixedInputs)),
       indent('FixedOutputs = ' + lstr(self.FixedOutputs)),
@@ -37,6 +54,21 @@ class ProcessNodeMetadata:
       indent('DynamicOperations = ' + lstr(self.DynamicOperations)),
       indent('DynamicGlobalRefs = ' + lstr(self.DynamicGlobalRefs)),
     '}'))
+  
+  def signature(self):
+    FixedOperationsCount = len(self.FixedOperations)
+    DynamicOperationsCount = len(self.DynamicOperations)
+    FixedInputsCount = len(self.FixedInputs)
+    DynamicInputsCount = len(self.DynamicInputs)
+    FixedImpulsesCount = len(self.FixedImpulses)
+    DynamicImpulsesCount = len(self.DynamicImpulses)
+    FixedOutputsCount = len(self.FixedOutputs)
+    DynamicOutputsCount = len(self.DynamicOutputs)
+    FixedReferencesCount = len(self.FixedReferences)
+    FixedGlobalRefsCount = len(self.FixedGlobalRefs)
+    DynamicGlobalRefsCount = len(self.DynamicGlobalRefs)
+    return (FixedOperationsCount, DynamicOperationsCount, FixedInputsCount, DynamicInputsCount, FixedImpulsesCount, DynamicImpulsesCount, FixedOutputsCount, DynamicOutputsCount, FixedReferencesCount, FixedGlobalRefsCount, DynamicGlobalRefsCount)
+
 
 class ProcessInputMetadata:
   def __init__(self, meta):
@@ -51,12 +83,13 @@ class ProcessInputMetadata:
     self.IsPotentiallyListeningToChanges = meta.IsPotentiallyListeningToChanges
     self.DataClass = ProcessDataClass(meta.DataClass)
     self.InputType = ProcessType(meta.InputType)
+    
+    assertfieldattributes(self,  [6, 38])
+    assertfieldtype(self, 'InputType')
   
   def __str__(self):
     return '\n'.join(('InputMetadata {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
-      indent('Field = ' + str(self.Field)),
       indent('DefaultValue = ' + str(self.DefaultValue)),
       indent('IsConditional = ' + str(self.IsConditional)),
       indent('IsListeningToChanges = ' + str(self.IsListeningToChanges)),
@@ -77,12 +110,13 @@ class ProcessOutputMetadata:
     self.OutputType = ProcessType(meta.OutputType)
     self.DataClass = ProcessDataClass(meta.DataClass)
     self.IsImplicit = meta.IsImplicit
+    
+    assertfieldattributes(self,  38)
+    assertfieldtype(self, 'OutputType')
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
-      indent('Field = ' + str(self.Field)),
       indent('ChangeTypeEval = ' + str(self.ChangeTypeEval)),
       indent('ChangeType = ' + str(self.ChangeType)),
       indent('OutputType = ' + str(self.OutputType)),
@@ -96,13 +130,14 @@ class ProcessImpulseMetadata:
     self.Name = meta.Name
     self.Type = ProcessImpulseType(meta.Type)
     self.Field = ProcessFieldInfo(meta.Field)
+    
+    assert self.Field.FieldType.FullName == 'ProtoFlux.Core.' + self.Type
+    assertfieldattributes(self,  [6, 38])
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
       indent('Type = ' + str(self.Type)),
-      indent('Field = ' + str(self.Field)),
     '}'))
 
 class ProcessOperationMetadata:
@@ -112,12 +147,13 @@ class ProcessOperationMetadata:
     self.Field = ProcessFieldInfo(meta.Field)
     self.IsSelf = meta.IsSelf
     self.IsAsync = meta.IsAsync
+    
+    assert self.Field is None or self.Field.FieldType.FullName == 'ProtoFlux.Core.Operation'
+    assertfieldattributes(self,  38)
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
-      indent('Field = ' + str(self.Field)),
       indent('IsSelf = ' + str(self.IsSelf)),
       indent('IsAsync = ' + str(self.IsAsync)),
     '}'))
@@ -128,13 +164,14 @@ class ProcessReferenceMetadata:
     self.Name = meta.Name
     self.ReferenceType = ProcessType(meta.ReferenceType)
     self.Field = ProcessFieldInfo(meta.Field)
+    
+    assertfieldattributes(self,  [6, 38])
+    assertfieldtype(self, 'ReferenceType')
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
       indent('ReferenceType = ' + str(self.ReferenceType)),
-      indent('Field = ' + str(self.Field)),
     '}'))
 
 class ProcessGlobalRefMetadata:
@@ -143,13 +180,14 @@ class ProcessGlobalRefMetadata:
     self.Name = meta.Name
     self.ValueType = ProcessType(meta.ValueType)
     self.Field = ProcessFieldInfo(meta.Field)
+    
+    assertfieldattributes(self,  38)
+    assertfieldtype(self, 'ValueType')
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
       indent('ValueType = ' + str(self.ValueType)),
-      indent('Field = ' + str(self.Field)),
     '}'))
 
 class ProcessInputListMetadata:
@@ -165,12 +203,13 @@ class ProcessInputListMetadata:
     self.IsPotentiallyListeningToChanges = meta.IsPotentiallyListeningToChanges
     self.TypeConstraint = ProcessType(meta.TypeConstraint)
     self.DataClassConstraint = ProcessDataClass(meta.DataClassConstraint)
+    
+    assertfieldattributes(self, 38)
+    assertfieldtype(self, 'TypeConstraint')
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
-      indent('Field = ' + str(self.Field)),
       indent('DefaultValue = ' + str(self.DefaultValue)),
       indent('IsConditional = ' + str(self.IsConditional)),
       indent('IsListeningToChanges = ' + str(self.IsListeningToChanges)),
@@ -190,12 +229,13 @@ class ProcessOutputListMetadata:
     self.ChangeType = ProcessOutputChangeSource(meta.ChangeType)
     self.TypeConstraint = ProcessType(meta.TypeConstraint)
     self.DataClassConstraint = ProcessDataClass(meta.DataClassConstraint)
+    
+    assertfieldattributes(self, 38)
+    assertfieldtype(self, 'TypeConstraint')
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
-      indent('Field = ' + str(self.Field)),
       indent('ChangeTypeEval = ' + str(self.ChangeTypeEval)),
       indent('ChangeType = ' + str(self.ChangeType)),
       indent('TypeConstraint = ' + str(self.TypeConstraint)),
@@ -208,13 +248,14 @@ class ProcessImpulseListMetadata:
     self.Name = meta.Name
     self.Type = ProcessImpulseType(meta.Type)
     self.Field = ProcessFieldInfo(meta.Field)
+    
+    assert self.Field.FieldType.FullName == 'ProtoFlux.Core.' + self.Type + 'List'
+    assertfieldattributes(self, 38)
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
       indent('Type = ' + str(self.Type)),
-      indent('Field = ' + str(self.Field)),
     '}'))
 
 class ProcessOperationListMetadata:
@@ -224,12 +265,13 @@ class ProcessOperationListMetadata:
     self.Field = ProcessFieldInfo(meta.Field)
     self.SupportsSync = meta.SupportsSync
     self.SupportsAsync = meta.SupportsAsync
+    
+    assert self.Field.FieldType.FullName == 'ProtoFlux.Core.' + (("MixedOperation" if self.SupportsAsync else "SyncOperation") if self.SupportsSync else ("AsyncOperation" if self.SupportsAsync else "NoneOperation")) + 'List'
+    assertfieldattributes(self, 38)
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
-      indent('Field = ' + str(self.Field)),
       indent('SupportsSync = ' + str(self.SupportsSync)),
       indent('SupportsAsync = ' + str(self.SupportsAsync)),
     '}'))
@@ -239,12 +281,12 @@ class ProcessGlobalRefListMetadata:
     self.Index = meta.Index
     self.Name = meta.Name
     self.Field = ProcessFieldInfo(meta.Field)
+    
+    assertfieldattributes(self, 38)
   
   def __str__(self):
     return '\n'.join((' {',
-      indent('Index = ' + str(self.Index)),
       indent('Name = ' + str(self.Name)),
-      indent('Field = ' + str(self.Field)),
     '}'))
 
 def ProcessDataClass(c):
